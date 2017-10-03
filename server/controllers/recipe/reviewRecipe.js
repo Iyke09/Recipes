339ee5aa05041fd958462469eaf3a08aba87773e
Emma Paris <iykay33@gmail.com>
2017-10-03 19:02:04 +0100

    add user pages

--- server/controllers/recipe/reviewRecipe.js
+++ server/controllers/recipe/reviewRecipe.js
@@ -22,7 +22,7 @@ const reviewRecipe = (req, res) => { // ---------------------add review and aler
         });
       }
         Comment.create({
-          recipeId: 1,
+          recipeId: 25,
           content: req.body.content,
           email: decoded.user.username,
           occupation: req.params.id,
